537d0e4b6c20453315bdf716f495fd987fac21e8
EmpiricistSkeptic <131171004+EmpiricistSkeptic@users.noreply.github.com>
2024-12-27 16:47:09 +0000

    s

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main_page, name='main_page')
+    path('', views.main_page, name='main_page'),
+    path('poems/', views.poems_page, name='poems_page'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def main_page(request):
     return render(request, 'main.html')
+
+def poems_page(request):
+    return render(request, 'poems.html')
