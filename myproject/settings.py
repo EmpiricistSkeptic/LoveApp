95251bd9a0da7c5f167ba46975fe7f767b3d3013
EmpiricistSkeptic <131171004+EmpiricistSkeptic@users.noreply.github.com>
2024-12-31 17:42:06 +0000

    s

--- myproject/settings.py
+++ myproject/settings.py
@@ -1,4 +1,5 @@
-import django_heroku 
+import django_heroku
+import os
 
 from pathlib import Path
 
@@ -107,7 +108,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# Статические файлы в проекте
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "main", "static"),  # Папка для статики в проекте
+]
+
+# Место для хранения собранных статических файлов
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 # Default primary key field type
