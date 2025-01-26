d2121785279f1fe4f6461e41d7ab8315a95eb030
EmpiricistSkeptic <131171004+EmpiricistSkeptic@users.noreply.github.com>
2025-01-26 14:27:00 +0000

    cache manifest

--- main/static/js/sw.js
+++ main/static/js/sw.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = 'v3'; // Меняйте версию при обновлении файлов!
+const CACHE_NAME = 'v4'; // Меняйте версию при обновлении файлов!
 
 const ASSETS = [
   '/', 
   '/static/css/main.css', 
   '/static/js/main.js',
+  '/static/manifest.json',
   '/static/icons/android-chrome-192x192.png', // Добавьте сюда иконки
   '/static/icons/android-chrome-512x512.png'
 ];
